87be5788a9f9903c0b0dce128d47cc7e011e62e7
vecchio <weichao@zbj.com>
2017-10-16 20:25:50 +0800

    todo:modify some files like hosts that belongs to root or other

--- app/main/modules/config-manage/index.js
+++ app/main/modules/config-manage/index.js
@@ -8,6 +8,9 @@ const {
     ipcMain
 } = electron
 
+const fs = require('fs')
+const execSync = require('child_process').execSync
+
 ipcMain.on(`${ID}-add`, (event, pathes) => {
     if(pathes.length === 0) return
     const savedPathesConnection = db.get('savePathes')
@@ -20,6 +23,19 @@ ipcMain.on(`${ID}-add`, (event, pathes) => {
 
 ipcMain.on(`${ID}-fetch`, (event) => {
     event.sender.send(`${ID}-fetch-cb`, db.get('savePathes').value())
+})
+
+ipcMain.on(`${ID}-fetchFileContent`, (event, pathLink) => {
+    fs.readFile(pathLink, (err, file) => {
+        event.sender.send(`${ID}-fetchFileContent-cb`, file)
+    })
+})
+
+ipcMain.on(`${ID}-changeFileContent`, (event, filePath, fileContent) => {
+    fs.writeFile(filePath, fileContent, (err) => {
+        if(!err) return
+        else console.log(err)
+    })
 })
 
 // 初始化数据库
--- app/ui/containers/config-manage.js
+++ app/ui/containers/config-manage.js
@@ -1,4 +1,5 @@
 /**
+ * 每个模块给一个 ID 标识
  * id: configManage
  */
 'use strict'
@@ -11,22 +12,48 @@ const ID = 'configManage'
 const electron = require('electron')
 const { ipcRenderer, remote } = electron
 const { dialog } = remote
-const path = require('path')
+const basename = require('path').basename
 
 export default class Test extends Component {
+    /**
+     * 原生
+     */
     constructor(props) {
         super(props)
         this.state = {
-            savePathes: []
+            savedPathes: {},
+            selectFilePath: '',
+            fileContent: ''
         }
     }
+    componentDidMount() {
+        this.register()
+        this.fetchList()
+    }
+    /**
+     * 扩展
+     */
     callDialog() {
         dialog.showOpenDialog({
             title: '请选择添加的配置文件',
             properties: ['openFile', 'multiSelections']
         }, pathes => {
+            if(!pathes) return
             ipcRenderer.send(`${ID}-add`, pathes)
+            pathes.map(path => {
+                this.state.savedPathes[path] = true
+            })
+            this.setState({
+                savedPathes: this.state.savedPathes
+            })
         })
+    }
+    fetchFileContent(pathItem) {
+        const path = pathItem.key
+        this.setState({
+            selectFilePath: path
+        })
+        ipcRenderer.send(`${ID}-fetchFileContent`, path)
     }
     fetchList() {
         ipcRenderer.send(`${ID}-fetch`)
@@ -34,32 +61,53 @@ export default class Test extends Component {
     register() {
         ipcRenderer.on(`${ID}-fetch-cb`, (event, pathList) => {
             this.setState({
-                savePathes: pathList
+                savedPathes: pathList
+            })
+        })
+        ipcRenderer.on(`${ID}-fetchFileContent-cb`, (event, fileContent) => {
+            this.setState({
+                fileContent
             })
         })
     }
-    componentDidMount() {
-        this.register()
-        this.fetchList()
+    saveFileChange() {
+        ipcRenderer.send(`${ID}-changeFileContent`, this.state.selectFilePath, this.state.fileContent)
     }
     render() {
+        const genSavedPathesList = (pathes) => {
+            let pathList = []
+            for(let path in pathes) {
+                if(pathes[path] === true) {
+                    pathList.push(
+                        <Menu.Item key={ path } title={ path }>
+                            { basename(path) }
+                        </Menu.Item>
+                    )
+                }
+            }
+            return pathList
+        }
         return (
             <div className='con-config-manage'>
                 <Row gutter={16}>
                     <Col span={4}>
                         <Input.Search onSearch={ value => console.log(value) } />
-                        <Menu theme='dark'>
-                            <Menu.Item>菜单项</Menu.Item>
-                            <Menu.Item>
-                                { this.state.savedPathes }
-                            </Menu.Item>
+                        <Menu theme='dark' onSelect={ (pathItem) => this.fetchFileContent(pathItem) }>
+                            { genSavedPathesList(this.state.savedPathes) }
                         </Menu>
-                        <Button type='primary' onClick={ this.callDialog }>
-                            asd
+                        <Button type='primary' onClick={ () => this.callDialog() }>
+                            新增
                         </Button>
+                        {
+                            this.state.fileContent
+                            ? <Button onClick={ () => this.saveFileChange() }>
+                                保存
+                              </Button>
+                            : ''
+                        }
                     </Col>
                     <Col span={20}>
-                        <Input.TextArea rows="45" />
+                        <Input.TextArea rows="45" value={ this.state.fileContent } onChange={ (e) => { this.setState({ fileContent: e.target.value })} }/>
                     </Col>
                 </Row>
             </div>
